a745b53afe841d368b0bee6557c529f51235a7cb
Claudio9616 <clares9611@gmail.com>
2024-01-11 20:12:51 +0100

    add gamehover and points

--- script.js
+++ script.js
@@ -21,11 +21,28 @@
 // 1
 const button = document.getElementById('pulsante')
 const grid = document.getElementById('grid')
+const points = document.getElementById('punteggio')
 // 2
 const rows = 10
 const cols = 10
 // 3
 const totCells = rows * cols
+const bombs = 16
+const maxPoints = totCells - bombs
+let score = 0
+points.innerText = score
+// non mi resetta la score
+let isGameHover = false 
+const maxBombs = createBombs(totCells, bombs)
+console.log(maxBombs)
+function createBombs (maxBombs, bombs){
+    const totalBombs = []
+    while (totalBombs.length < bombs) {
+        const randomBombs = Math.floor(Math.random() * maxBombs) + 1
+        if(!totalBombs.includes(randomBombs)) totalBombs.push(randomBombs)   
+    }
+    return totalBombs
+}
 // 4
 const createCells = content => {
     // 4b
@@ -45,11 +62,33 @@ button.addEventListener('click', function(){
     // 6
     for (let i = 1; i <= totCells; i++){
         // 6b
-        const newCells = createCells(i)        
+        const newCells = createCells(i)      
+        
         // 7
         newCells.addEventListener('click', () => {
-           newCells.classList.toggle('clicca')
-           console.log(newCells)
+            if (isGameHover || newCells.classList.contains('clicca')){
+                return
+            }
+           newCells.classList.add('clicca')
+           const hasHitBomb = maxBombs.includes(parseInt(newCells.innerText))
+        //    chiedere il perchè di questa Cosa, se si usa al posto della i ok l'ho capito, ma perchè bisogna mettere la i o in questo caso specifico la newcell?
+        // cioè come faccio cosi facendo a vedere le celle?
+           if(hasHitBomb){
+            isGameHover = true
+            newCells.classList.add('bomba')
+            // non mi aggiunge la classe.....
+            console.log('hai pestato una bomba, il tuo punteggio è ' + score)
+           } else {
+                score++
+                points.innerText = score
+                console.log('score: ', score)
+                if (score === maxPoints){
+                    console.log('complimenti hai vinto')
+                    isGameHover = true
+                    // questo controllo lo faccio dentro l'else perchè, prima mi deve aumentare il punteggio con lo score++ e poi se quest'ultimo
+                    // // è uguale al maxPoints allora la flag diventa true e interrompo il gioco
+                }
+           }           
         })
         // 8
         grid.appendChild(newCells)        
